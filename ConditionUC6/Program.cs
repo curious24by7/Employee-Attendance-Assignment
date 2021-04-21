0ca69fef655ce522aaf5904c96a929791e0e8a22
sohamdhamnaskar <sohamdham123@gmail.com>
2021-04-21 10:39:17 +0530

    Added: Employee Attendace for month with wage.

--- ConditionUC6/Program.cs
+++ ConditionUC6/Program.cs
@@ -14,8 +14,33 @@ namespace ConditionUC6
             int MaxEmpWage = 100;
             int TotalWorkHrs = 0;
             int TotalWorkdays = 0;
-            
-            while ()
-        }
+
+            while (TotalWorkdays < 20 && TotalWorkHrs < MaxEmpWage)
+            {
+                TotalWorkdays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case 0:
+                        Console.WriteLine("Employee is Absent.");
+                        EmpHrs = 0;
+                        break;
+                    case 1:
+                        Console.WriteLine("Employee is Present Full Time.");
+                        EmpHrs = 8;
+                        break;
+                    case 2:
+                        Console.WriteLine("Employe is Present Part Time.");
+                        EmpHrs = 4;
+                        break;
+                }
+                TotalWorkHrs = TotalWorkHrs + EmpHrs;
+            }
+            EmpWage = TotalWorkHrs * EmpRatePerHr;
+
+            Console.WriteLine("Employee Wage:" + EmpWage);
+
+        }  
     }
 }
